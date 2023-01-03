3d2b8eaf63d5b3b58e696933edcb7c365291b7fd
Malthe Andersen <s194257@student.dtu.dk>
2023-01-03 14:29:12 +0100

    Update train_model.py

--- MLOPsclassifier/src/models/train_model.py
+++ MLOPsclassifier/src/models/train_model.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import click
 import numpy as np
 
-
+#load training data
 train_img = torch.load("data/processed/train_images_norm.pt")
 train_label = torch.load("data/processed/train_labels.pt")
 trainz = {'images':train_img,'labels':train_label}
@@ -15,6 +15,7 @@ from model import MyAwesomeModel
 
 # create trainLoader
 class tloader(Dataset):
+    #tloader class prepares the data for the DataLoader function from torch.
     def __init__(self, train):
         self.img_labels = train['labels']
         self.img = train['images']
@@ -39,20 +40,21 @@ def train(lr):
     print("Training day and night")
     print(lr)
 
-    model = MyAwesomeModel()
+    model = MyAwesomeModel()#load model configuration
     train_set = trainz
 
     trainloader = DataLoader(tloader(train_set), batch_size=64, shuffle=True)
 
+    #define loss function (criterion) and optimizer. I just choose these two because they usually work well.
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 
-    loss_list = []
-    epochs = 10
+    loss_list = [] #for saving loss for plotting (still need to make plot)
+    epochs = 10 #how many epochs to train, increase if you have a lot of time
     for e in range(epochs):
         running_loss = 0
-        for images, labels in trainloader:
-
+        for images, labels in trainloader: #loop over data
+            #standard training loop
             optimizer.zero_grad()
             output = model(images)
             loss = criterion(output, labels)
@@ -65,7 +67,7 @@ def train(lr):
         else:
             print(f"Training loss: {running_loss / len(trainloader)}")
 
-    torch.save(model.state_dict(), 'models/trained_model.pt')
+    torch.save(model.state_dict(), 'models/trained_model.pt') #save model state dict
 
 
 cli.add_command(train)
